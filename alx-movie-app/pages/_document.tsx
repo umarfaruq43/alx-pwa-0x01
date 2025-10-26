071a58a3704dbf630179d6b78db9f738aefc4add
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2025-10-26 22:37:32 +0100

    manifest  linking

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -1,13 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head />
+            <link rel="manifest" href="/manifest.json" />
+            <body className="antialiased">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
